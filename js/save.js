0a2fc0b31c235061fc2bfa44641fe03ea4ac92f3
Rohan Saraf <govind.1@iitj.ac.in>
2017-01-31 00:26:36 +0530

    Updated the save.js file

--- js/save.js
+++ js/save.js
@@ -1,7 +1,8 @@
 var values = {};
 var key1 = "username";
 var key2 = "password";
-
+var tablink="https://10.0.1.254:4100/logon.shtml";
+var success="https://10.0.1.254:4100/success.shtml"
 function saveOptions() {
   var username = document.getElementsByTagName('input')[0].value;
   var password = document.getElementsByTagName('input')[1].value;
@@ -9,6 +10,9 @@ function saveOptions() {
   values[key2]=password;
 /**/
   chrome.storage.local.set(values);
+  window.open(tablink,"_self");
+ // chrome.tabs.remove(0);
+  //chrome.tabs.create({url:tablink})
    // console.log("saved values are "+username" "+password);
 }
 
@@ -19,9 +23,12 @@ function restoreOptions() {
   });
     chrome.storage.local.get(key2 , function(items) {
     console.log(items.password);
-    document.getElementsByTagName('input')[1].value=items.password;
+   // document.getElementsByTagName('input')[1].value=items.password;
   });
 }
-
-//document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('Logout').addEventListener('click',function(){
+  window.open(success,"_self");
+  form.getElementsByTagName('input')[0].click();
+})
+document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click',saveOptions);
